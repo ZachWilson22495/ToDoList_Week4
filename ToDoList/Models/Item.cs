532f8dbeae6294362b5e7cdc59bfddc75f1938d8
Zach Wilson <zwilson22495@gmail.com>
2022-01-04 10:53:22 -0800

    Pushed to Github
    Co-Authored-by: Matt Luker <jamesmattluker@gmail.com>
    Co-Authored-by: Albert Lee <leesga8@gmail.com>

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -1,10 +1,20 @@
+using System.Collections.Generic;
+using System;
+
 namespace ToDoList.Models
 {
   public class Item
   {
+    public Item()
+    {
+    this.JoinEntities = new HashSet<CategoryItem>();
+    }
+
     public int ItemId { get; set; }
     public string Description { get; set; }
-    public int CategoryId { get; set; }
-    public virtual Category Category { get; set; }
+    public bool Completed { get; set; }
+    public DateTime DueDate { get; set; }
+
+    public virtual ICollection<CategoryItem> JoinEntities { get; }
   }
 }
